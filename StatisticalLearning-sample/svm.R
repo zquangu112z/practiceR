cb717d88772d8822bdcbbc79473ae785dc957b90
Ngữ Quang Trương <zquangu112z@gmail.com>
2018-03-12 09:28:32 +0700

    feat: add svm classifier

--- StatisticalLearning-sample/svm.R
+++ StatisticalLearning-sample/svm.R
@@ -1,3 +1,5 @@
+library(e1071)
+
 # generate observation
 set.seed(1)
 x <- matrix(rnorm(20 * 2), ncol = 2)
@@ -6,21 +8,21 @@ y <- c(rep(-1, 10), rep(1, 10))
 x[y == 1,] <- x[y == 1,] + 1
 x
 
-plot(x, col = (3 - y))
+plot(x, col = (3 - y)) # col means color
 dat = data.frame(x = x, y = as.factor(y))
-library(e1071)
+
 # When the cost argument is large,
 # then the margins will be narrow
 # and there will be few support vectors on the margin or violating the margin
 svmfit <- svm(
   y ~ .,
   data = dat,
-  kernel = "polynomial",
+  kernel = "linear",
   cost = 10,
   scale = FALSE
 )
 plot(svmfit , dat)
-
+svmfit$index
 summary(svmfit)
 
 set.seed(1)
@@ -35,3 +37,13 @@ summary(tune.out)
 # select the best model
 bestmod =tune.out$best.model
 summary(bestmod)
+xtest <- matrix(rnorm(20*2), ncol = 2)
+ytest <- sample(c(-1, 1), 20, replace = TRUE)
+xtest
+ytest
+xtest[ytest==1,] <- xtest[ytest==1, ] + 1
+testdat <- data.frame(x=xtest, y=as.factor(ytest))
+# predict 
+ypred <- predict(bestmod, testdat)
+table(ypred, ytest)
+
